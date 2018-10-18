54b57bc2b7a30c74f75bb3c090a79ff13c798f55
Lotus <cmadisonne@gmail.com>
2018-10-18 11:11:39 -0500

    Running

--- ProTwo/apptwo/views.py
+++ ProTwo/apptwo/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from appTwo.models import User
+from apptwo.models import User
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'apptwo/index.html')
+    return render(request, 'appTwo/index.html')
 
 
 def users(request):
     # THIS AINT WORKING
-    return render(request, 'apptwo/users.html')
+    return render(request, 'appTwo/users.html')
